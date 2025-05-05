6853ac608baef9f7934bcfc1331e82176e79b7cf
Aariful Islam Jaahid <oggysmac@Oggys-Mac.local>
2025-05-06 00:37:20 +0600

    Fix Test Case Failure Issue

--- tests/WebElements/DropdownMultiple.spec.js
+++ tests/WebElements/DropdownMultiple.spec.js
@@ -9,17 +9,11 @@
 
 const {test, expect} = require("@playwright/test")
 
-test('Handiling Dropdown List', async ({page}) => {
+test('Handiling Multiple Dropdown List', async ({page}) => {
 
     await page.goto('https://www.saucedemo.com/v1/index.html');
     await page.locator('//input[@id="user-name"]').fill('standard_user');
     await page.locator('//input[@id="password"]').fill('secret_sauce');
     await page.locator('//input[@id="login-button"]').click();
-    await page.locator('//select[@class="product_sort_container"]').selectOption(['wewq','wewq','qewq']);
-
-
-    await page.waitForTimeout(10000);
-
-    //await expect(page).toHaveURL('/inventory.html/');
-    
+    await page.locator('//select[@class="product_sort_container"]').selectOption(['az', 'za', 'lohi']);
 })
