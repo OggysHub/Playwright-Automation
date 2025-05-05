c678330590daa8d550ac80ca8f72f6db211138c3
Aariful Islam Jaahid <oggysmac@Oggys-Mac.local>
2025-05-06 00:35:55 +0600

    Fix the Basic Auth Failure Issue

--- tests/Authentication/BasicHttpAuthentication.spec.js
+++ tests/Authentication/BasicHttpAuthentication.spec.js
@@ -51,8 +51,6 @@ test('Basic Authentication', async({page})=>{
     });
 
     await page.goto('https://admin:admin@the-internet.herokuapp.com/basic_auth')
-    const text = await page.locator('dive[class="example"] p').textContent()
+    const text = await page.locator("//div[@class='example']").textContent()
     expect(text).toContain('Congratulations')
-
-    await page.waitForTimeout(10000);
 })
